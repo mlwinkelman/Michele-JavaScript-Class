3af8092aa2491b36051cf56d672461f5c9add3da
mlwinkelman <mlwinkelman@gmail.com>
2016-10-30 11:40:38 -0600

    add smooth scroll nav

--- app/pages/home.js
+++ app/pages/home.js
@@ -31,7 +31,20 @@ var app = {
         // console.log('Do something again xD.');
       });
     });
-
+    // smooth scroll to nav elements
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  
 
   }
 };
